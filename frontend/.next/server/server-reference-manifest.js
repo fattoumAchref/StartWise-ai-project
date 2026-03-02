583973cc81cba186dfc7586db6a8930470ebfc0f
mahdibani <bani_mehdi@hotmail.com>
2026-03-02 03:31:37 +0100

    fix shadcn classes

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZGSurGd//DEdQpsU5grskRw7q4vF3hDN9QF/OnBQYXs=\"\n}"
---
+++ frontend/.next/static/chunks/src_app_page_tsx_2c0c7d46._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_278cc974._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_19070a2b._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_f06f6792._.js",
+  "static/chunks/node_modules_next_b1945c53._.js",
+  "static/chunks/node_modules_18405aca._.js"
+],
+    source: "dynamic"
+});
